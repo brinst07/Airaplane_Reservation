acd9aa646770f87c59757b838f18bb5d4ceb0d82
park <brinst07@gmail.com>
2020-03-09 20:58:40 +0900

    도시삭제

--- src/dao/CityDao.java
+++ src/dao/CityDao.java
@@ -30,3 +30,4 @@ public class CityDao {
 		database.tb_city.add(cityvo);
 	}
 }
+ 
--- src/service/CityService.java
+++ src/service/CityService.java
@@ -1,6 +1,6 @@
 package service;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 import dao.CityDao;
 import vo.CityVO;
@@ -20,6 +20,7 @@ public class CityService {
 	
 	CityDao city = CityDao.getInstance();
 	CityVO cityvo = new CityVO();
+	Scanner sc = new Scanner(System.in);
 	
 	//city를 출력해주는 메소드
 	public void showCity(int cho) {
@@ -27,6 +28,29 @@ public class CityService {
 			CityVO excity = city.citylist().get(i);
 			if(excity.getCoun_num()==cho) {
 				System.out.println(excity.getCity_name());
+				
+			}
+		}
+	}
+	
+	//city를 추가해주는 메소드
+	public void insertCity(int cho) {
+		System.out.println("도시이름을 입력해주세요");
+		String cityname = sc.nextLine();
+		cityvo.setCity_name(cityname);
+		cityvo.setCoun_num(cho);
+		city.insertcity(cityvo);
+	}
+	
+	//city를 삭제해주는 메소드
+	public void deleteCity(int cho) {
+		System.out.println("도시이름을 입력해주세요");
+		String cityname = sc.nextLine();
+		for(int i=0; i<city.citylist().size(); i++) {
+			CityVO dcity = city.citylist().get(i);
+			if(dcity.getCity_name().equals(cityname)) {
+				city.citylist().remove(i);
+				System.out.println("성공적으로 삭제되었습니다.!");
 			}
 		}
 	}
