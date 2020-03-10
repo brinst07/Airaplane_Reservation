86f903a660410c38c5d57578065a08e03a2b1ced
이승민97 <97@LSM-NOTEBOOK>
2020-03-10 20:48:19 +0900

    날짜 형식 완성

--- src/controller/ReservationTicketController.java
+++ src/controller/ReservationTicketController.java
@@ -1,7 +1,7 @@
 package controller;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -46,19 +46,74 @@ public class ReservationTicketController {
       
       String date = " ";
       
+      Date today = new Date();      
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); //(2020-03-10) 형식으로 저장
+      String styear = sdf.format(today).substring(0,4); //연도만 빼오기
+      String stmon = sdf.format(today).substring(5,7); // 달만 빼오기
+      String stday = sdf.format(today).substring(8,10); // 일자만 빼오기
+      
+      int year = Integer.parseInt(styear); // 올해
+      int mon = Integer.parseInt(stmon); // 현재 월
+      int day = Integer.parseInt(stday); // 현재 일
+      
       while(true) {
          System.out.print("출발 날짜를 입력해주세요(yyyymmdd) : ");
          date = sc.nextLine();
          
-         if(date.length() != 8) {
+         Calendar cal = Calendar.getInstance();
+         
+         if(date.length() != 8) { // 20200310이 들어오면 길이가 8개 이지만 그렇지 않을 경우 형식이 맞지 않은 것으로 간주
             System.out.println("날짜 형식에 맞게 입력해주세요.\n");
             continue;
          }
          else {
-            String a = date.substring(0,4);
-            String b = date.substring(4,6);
-            String c = date.substring(6,8);
-            date = a + "-" + b + "-" + c;
+            String a = date.substring(0,4); // 연
+            String b = date.substring(4,6); // 월
+            String c = date.substring(6,8); // 일
+            
+            int a1 = Integer.parseInt(a); // 인트로 형변환
+            int b1 = Integer.parseInt(b);
+            int c1 = Integer.parseInt(c);          
+            
+            cal.set(a1,b1-1,c1); // 날짜형식으로 저장
+            
+            String test = sdf.format(cal.getTime()); // 20200532 처럼 존재하지 않는 날짜가 입력 되면 0601로 자동 변환 되기 때문에 그런 것을 막기 위해 월을 비교하기 위함
+            String test1 = test.substring(0,4);
+            String test2 = test.substring(5,7);
+            String test3 = test.substring(8,10);
+            
+            int a2 = Integer.parseInt(test1);
+            int b2 = Integer.parseInt(test2);   
+            int c2 = Integer.parseInt(test3);            
+            
+            if(year > a2) { // 연도가 작을 경우
+            	System.out.println("현재 날짜보다 이전 날짜는 입력하실 수 없습니다.");
+            	continue;
+            }
+            
+            if(year >= a2) { // 연도가 같거나 작으면서 월이 작을 경우
+            	if(mon > b2) {
+            		System.out.println("현재 날짜보다 이전 날짜는 입력하실 수 없습니다.");
+            		continue;
+            	}
+            }
+            
+            if(year >= a2) { // 일자가 작을 경우
+            	if(mon >= b2) {
+            		if(day > c2) {
+            			System.out.println("현재 날짜보다 이전 날짜는 입력하실 수 없습니다.");
+            			continue;
+            		}
+            	}
+            }
+            
+            if(b1==b2) { // 날짜가 제대로 입력 된 경우
+            	date = sdf.format(cal.getTime());
+            }
+            else {
+            	System.out.println("잘못된 날짜 형식입니다.");
+            	continue;
+            }
             break;
          }
       }
