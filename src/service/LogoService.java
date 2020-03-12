42ffc8dbb64b0779dcb6934ced651fcfbfc02017
LeeSeungMin <ching45618@gmail.com>
2020-03-12 14:19:42 +0900

    로고서비스 클래스 추가

---
+++ src/service/LogoService.java
@@ -0,0 +1,58 @@
+package service;
+
+public class LogoService {
+
+	public void logo() {
+		CleardelayService cds2 = new CleardelayService();
+
+		System.out.println(
+				"               AAA                 iiii                   PPPPPPPPPPPPPPPPP    lllllll                                                        ");
+		cds2.delay(300);
+		System.out.println(
+				"              A:::A               i::::i                  P::::::::::::::::P   l:::::l                                                        ");
+		cds2.delay(300);
+		System.out.println(
+				"             A:::::A               iiii                   P::::::PPPPPP:::::P  l:::::l                                                        ");
+		cds2.delay(300);
+		System.out.println(
+				"            A:::::::A                                     PP:::::P     P:::::P l:::::l                                                        ");
+		cds2.delay(300);
+		System.out.println(
+				"           A:::::::::A           iiiiiii rrrrr   rrrrrrrrr   P::::P     P:::::P l::::l   aaaaaaaaaaaaa  nnnn  nnnnnnnn        eeeeeeeeeeee    ");
+		cds2.delay(300);
+		System.out.println(
+				"          A:::::A:::::A          i:::::i r::::rrr:::::::::r  P::::P     P:::::P l::::l   a::::::::::::a n:::nn::::::::nn    ee::::::::::::ee  ");
+		cds2.delay(300);
+		System.out.println(
+				"         A:::::A A:::::A          i::::i r:::::::::::::::::r P::::PPPPPP:::::P  l::::l   aaaaaaaaa:::::an::::::::::::::nn  e::::::eeeee:::::ee");
+		cds2.delay(300);
+		System.out.println(
+				"        A:::::A   A:::::A         i::::i rr::::::rrrrr::::::rP:::::::::::::PP   l::::l            a::::ann:::::::::::::::ne::::::e     e:::::e");
+		cds2.delay(300);
+		System.out.println(
+				"       A:::::A     A:::::A        i::::i  r:::::r     r:::::rP::::PPPPPPPPP     l::::l     aaaaaaa:::::a  n:::::nnnn:::::ne:::::::eeeee::::::e");
+		cds2.delay(300);
+		System.out.println(
+				"      A:::::AAAAAAAAA:::::A       i::::i  r:::::r     rrrrrrrP::::P             l::::l   aa::::::::::::a  n::::n    n::::ne:::::::::::::::::e ");
+		cds2.delay(300);
+		System.out.println(
+				"     A:::::::::::::::::::::A      i::::i  r:::::r            P::::P             l::::l  a::::aaaa::::::a  n::::n    n::::ne::::::eeeeeeeeeee  ");
+		cds2.delay(300);
+		System.out.println(
+				"    A:::::AAAAAAAAAAAAA:::::A     i::::i  r:::::r            P::::P             l::::l a::::a    a:::::a  n::::n    n::::ne:::::::e           ");
+		cds2.delay(300);
+		System.out.println(
+				"   A:::::A             A:::::A   i::::::i r:::::r          PP::::::PP          l::::::la::::a    a:::::a  n::::n    n::::ne::::::::e          ");
+		cds2.delay(300);
+		System.out.println(
+				"  A:::::A               A:::::A  i::::::i r:::::r          P::::::::P          l::::::la:::::aaaa::::::a  n::::n    n::::n e::::::::eeeeeeee  ");
+		cds2.delay(300);
+		System.out.println(
+				" A:::::A                 A:::::A i::::::i r:::::r          P::::::::P          l::::::l a::::::::::aa:::a n::::n    n::::n  ee:::::::::::::e  ");
+		cds2.delay(300);
+		System.out.println(
+				"AAAAAAA                   AAAAAAAiiiiiiii rrrrrrr          PPPPPPPPPP          llllllll  aaaaaaaaaa  aaaa nnnnnn    nnnnnn    eeeeeeeeeeeeee  ");
+		cds2.delay(1000);
+	}
+
+}
